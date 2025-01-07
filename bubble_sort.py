45153b4cd684490013733ffa03f1f8609b39cd8d
jamie <133122446+jdrgw@users.noreply.github.com>
2025-01-07 15:47:41 +0000

    Add conventional bubble sort function to file

--- bubble_sort.py
+++ bubble_sort.py
@@ -53,3 +53,52 @@ def bad_bubble_sort(l: list, reverse: bool = False) -> list:
 
     
     return l
+
+def bubble_sort(l: list, reverse: bool=False) -> list:
+    """
+    Sorts a list using the bubble sort algorithm.
+
+    Args:
+        l (list): The list to be sorted.
+        reverse (bool, optional): If True, sorts the list in descending order. 
+                                  If False (default), sorts in ascending order.
+
+    Returns:
+        list: The sorted list.
+
+    Raises:
+        Exception: If the 'reverse' argument is not a boolean.
+        Exception: If 'l' is not a list.
+
+    Example:
+        >>> bubble_sort([3, 1, 4, 1, 5, 9], reverse=False)
+        [1, 1, 3, 4, 5, 9]
+
+        >>> bubble_sort([3, 1, 4, 1, 5, 9], reverse=True)
+        [9, 5, 4, 3, 1, 1]
+    """
+
+    if not isinstance(reverse, bool):
+        raise Exception("reverse must be True or False")
+
+    if not isinstance(l, list):
+        raise Exception("This function takes a list")
+    
+    for i in range(len(l)):
+        swapped = False
+        for j in range(1, len(l)-i):
+            if not reverse:
+                if l[j] < l[j-1]:
+                    l[j], l[j-1] = l[j-1], l[j]
+                    swapped = True
+
+
+            else:
+                if l[j] > l[j-1]:
+                    l[j-1], l[j] = l[j], l[j-1]
+                    swapped = True
+                    
+        if not swapped:
+            break
+        
+    return l
